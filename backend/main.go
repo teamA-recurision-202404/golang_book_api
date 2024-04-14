49cb51a65de136920390998579c138fb8779bed2
ryusei <ryusei@ubuntu22.04.myguest.virtualbox.org>
2024-04-14 17:16:01 +0900

    delete code

--- backend/main.go
+++ backend/main.go
@@ -9,23 +9,6 @@ import (
 	"example.com/zipcode_api_202304/pkg/handlers"
 )
 
-func helloHandler(w http.ResponseWriter, r *http.Request) {
-    // クエリパラメータを解析する
-    query := r.URL.Query()
-    name := query.Get("name") // "名前"から"name"へ修正
-
-    // レスポンス用のマップを作成
-    response := map[string]string{
-        "message": "Hello " + name, // “message”： "Hello " + name、から修正
-    }
-
-    // Content-Typeヘッダーをapplication/jsonに設定
-    w.Header().Set("Content-Type", "application/json")
-
-    // マップをJSONにエンコードしてレスポンスとして送信
-    json.NewEncoder(w).Encode(response)
-}
-
 func main() {
     fmt.Println("Starting the server!")
 
