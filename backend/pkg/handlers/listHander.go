15e6f8a8cd1a5a7010d17033e0955ec938fd8ab5
kashmii <www.myac@gmail.com>
2024-04-18 22:59:21 +0900

    レスポンスをjsonにする・リファクタリング

--- backend/pkg/handlers/listHander.go
+++ backend/pkg/handlers/listHander.go
@@ -3,11 +3,17 @@ package handlers
 import (
 	"encoding/json"
 	"fmt"
-	"io/ioutil"
+	"io"
 	"net/http"
 )
 
-type postcode struct {
+// レスポンスをjsonに変換するための構造体
+// 参考: https://randomuser.me/documentation#results
+type ResponseList struct {
+	Results []Postcode `json:"results"`
+}
+
+type Postcode struct {
 	Jis        string `json:"jis"`
 	OldZip     string `json:"old"`
 	Zip        string `json:"new"`
@@ -19,16 +25,8 @@ type postcode struct {
 func ListHandler(w http.ResponseWriter, r *http.Request) {
 	query := r.URL.Query()
 	page := query.Get("page")
-	fmt.Fprintf(w, "Hello World, from ListHandler!\n")
 
-	var response *http.Response
-	var err error
-
-	if page == "" {
-		response, err = http.Get("https://postcode.teraren.com/postcodes.json")
-	} else {
-		response, err = http.Get("https://postcode.teraren.com/postcodes.json?page=" + page)
-	}
+	response, err := http.Get("https://postcode.teraren.com/postcodes.json?page=" + page)
 
 	if err != nil {
 		fmt.Println("Error fetching data:", err)
@@ -37,24 +35,23 @@ func ListHandler(w http.ResponseWriter, r *http.Request) {
 
 	defer response.Body.Close()
 
-	body, err := ioutil.ReadAll(response.Body)
+	body, err := io.ReadAll(response.Body)
 	if err != nil {
 		return
 	}
 
-	var postcodes []postcode
+	var postcodes []Postcode
 	err = json.Unmarshal(body, &postcodes)
 	if err != nil {
 		return
 	}
 
-	err = json.NewEncoder(w).Encode(postcodes)
+	err = json.NewEncoder(w).Encode(ResponseList{Results: postcodes})
+
 	if err != nil {
 		http.Error(w, "Failed to encode JSON", http.StatusInternalServerError)
 		return
 	}
 
 	w.Header().Set("Content-Type", "application/json")
-	fmt.Fprintf(w, "%s", postcodes)
-
 }
