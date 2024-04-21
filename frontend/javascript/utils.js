fdbe7b26ab0cffe474d7aa16f36bddb40617bed0
ryusei <ryusei@ubuntu22.04.myguest.virtualbox.org>
2024-04-21 23:01:57 +0900

    revise code

--- frontend/javascript/utils.js
+++ frontend/javascript/utils.js
@@ -3,6 +3,48 @@ function convertPostcode(postcode) {
   return postcode.slice(0, 3) + '-' + postcode.slice(3);
 }
 
+function make_list(list) {
+  // 作成されるtr要素の中身
+  // <tr>
+  //  <th scope="row">1</th>
+  //  <td>111-2222</td>
+  //  <td>dummy</td>
+  //  <td>New York</td>
+  //  <td>Brooklyn</td>
+  //  <td><button id="detail" class="search btn btn-success">詳細</button></td>
+  // </tr>
+
+  tbody.innerHTML = '';
+  for (let i = 0; i < list.length; ++i) {
+    const tr = document.createElement('tr');
+    const th = document.createElement('th');
+    const td1 = document.createElement('td');
+    const td2 = document.createElement('td');
+    const td3 = document.createElement('td');
+    const td4 = document.createElement('td');
+    const td5 = document.createElement('td');
+    const button = document.createElement('button');
+
+    th.textContent = i + 1;
+    th.scope = 'row';
+    td1.textContent = convertPostcode(list[i].new);
+    td2.textContent = list[i].prefecture;
+    td3.textContent = list[i].city;
+    td4.textContent = list[i].suburb;
+    button.textContent = '詳細';
+    button.classList.add('detail', 'btn', 'btn-success');
+
+    tr.appendChild(th);
+    tr.appendChild(td1);
+    tr.appendChild(td2);
+    tr.appendChild(td3);
+    tr.appendChild(td4);
+    tr.appendChild(td5);
+    td5.appendChild(button);
+    tbody.appendChild(tr);
+  }
+}
+
 // === 検索ボタンを押した時の処理 ===
 
 async function fetchSearch() {
