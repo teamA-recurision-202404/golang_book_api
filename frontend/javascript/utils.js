f5cd457b42db5586068912a143623136b282e73b
ryusei <ryusei@ubuntu22.04.myguest.virtualbox.org>
2024-04-26 23:43:09 +0900

    devide function

--- frontend/javascript/utils.js
+++ frontend/javascript/utils.js
@@ -49,3 +49,47 @@ async function fetchDetail(value) {
     console.error('Error:', error);
   }
 }
+
+function make_list(list) {
+  // 作成されるtr要素の中身
+  // <tr>
+  //  <th scope="row">1</th>
+  //  <td>111-2222</td>
+  //  <td>dummy</td>
+  //  <td>New York</td>
+  //  <td>Brooklyn</td>
+  //  <td><button id="detail" class="search btn btn-success">詳細</button></td>
+  // </tr>
+
+  tbody.innerHTML = '';
+  for (let i = 0; i < list.length; ++i) {
+    const tr = document.createElement('tr');
+    const th = document.createElement('th');
+    const td1 = document.createElement('td');
+    const td2 = document.createElement('td');
+    const td3 = document.createElement('td');
+    const td4 = document.createElement('td');
+    const td5 = document.createElement('td');
+    const button = document.createElement('button');
+
+    th.textContent = i + 1;
+    th.scope = 'row';
+    td1.textContent = convertPostcode(list[i].postcode);
+    td2.textContent = list[i].prefecture;
+    td3.textContent = list[i].city;
+    td4.textContent = list[i].suburb;
+    button.textContent = '詳細';
+    button.classList.add('detail', 'btn', 'btn-success');
+    button.value = list[i].new;
+    button.value = list[i].postcode;
+
+    tr.appendChild(th);
+    tr.appendChild(td1);
+    tr.appendChild(td2);
+    tr.appendChild(td3);
+    tr.appendChild(td4);
+    tr.appendChild(td5);
+    td5.appendChild(button);
+    tbody.appendChild(tr);
+  }
+}
